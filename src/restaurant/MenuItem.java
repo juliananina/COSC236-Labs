bd2911379dcc302a8ac66b84c3cb4e5837da6a90
Christian Casey <christian.b.casey@gmail.com>
2024-10-17 15:23:24 -0700

    Added comments

--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -2,10 +2,13 @@ package restaurant;
 // MenuItem class
 public class MenuItem {
     private String name;
+    // changed price to priceInDollars
     private double priceInDollars;
 
+    // changed price to priceInDollars
     public MenuItem(String name, double priceInDollars) {
         this.name = name;
+	// changed price to priceInDollars
         this.priceInDollars = priceInDollars;
     }
 
@@ -14,6 +17,7 @@ public class MenuItem {
     }
 
     public double getPrice() {
+	// changed price to priceInDollars
         return priceInDollars;
     }
 }
