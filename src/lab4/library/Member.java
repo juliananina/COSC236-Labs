9a6880aafc01a575227c92a939044e5bbe7073a2
Foochini <zanetessmer@gmail.com>
2024-10-29 10:59:04 -0700

    Updated member.java

--- src/lab4/library/Member.java
+++ src/lab4/library/Member.java
@@ -1,13 +1,36 @@
 package lab4.library;
 
+import java.util.LinkedList;
+
 public class Member {
 
 	// GRASP Principle: Information Expert and Low Coupling
 	// Can borrow and return books.
-
+	private int memberId;
 	private String name;
-	// private borrowedbBooks TODO: implement collection of borrowed books
+	private LinkedList<Book> borrowedBooks;
+	//TODO: implement collection of borrowed books
 	
 	// TODO: implement functionality of Member class
+	public Member(int memberId, String name){
+		this.memberId = memberId;
+		this.name = name;
+		this.borrowedBooks = new LinkedList<Book>();
+	}
+	public void borrowBook(Book b) {
+		borrowedBooks.add(b);
+	}
+	public void returnBook(Book b) {
+		borrowedBooks.remove(b);
+	}
+	public int getmemberId() {
+		return memberId;
+	}
+	public String getName() {
+		return name;
+	}
+	public LinkedList<Book> getBorrowedBooks(){
+		return borrowedBooks;
+	}
 
 }
